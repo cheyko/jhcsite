f9125a6babde6dcc446da5d93af78f001306f8e6
Ariel Wilson <ariel.wilson.jc@gmail.com>
2021-08-24 15:15:17 -0500

    test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -67,7 +67,7 @@ const HomePage = props => {
   const CardBody = "Opening remarks .... "
 
   const postings = props.context.postings ? (props.context.postings > 5 ? (props.context.postings.slice(0,6)) : props.context.postings ) : null ;
-  console.log(postings.length);
+  //console.log(postings.length);
   
   return (
     <>
@@ -127,7 +127,7 @@ const HomePage = props => {
                     <br></br>
                     <br></br>
                     <div className="column columns is-multiline is-mobile">
-                      {postings !== undefined && postings.length ? (
+                      {postings && postings > 0 ? (
                         postings.map((posting, index) => (
                           <PostingItem
                           posting={posting}
