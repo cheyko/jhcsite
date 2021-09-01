40dc5cc40c90035ead46d56d838d8c4b479211c8
Ariel Wilson <ariel.wilson.jc@gmail.com>
2021-09-01 00:02:36 -0500

    removed lowercase methods from login route

--- src/components/Login.js
+++ src/components/Login.js
@@ -50,7 +50,7 @@ class Login extends Component {
           </nav>
         </div>
         <br />
-        <form action="/api/login" method="post" onSubmit={this.login}>
+        <form onSubmit={this.login}>
           <div className="columns is-mobile is-centered">
             <div className="box column grnBkgn is-one-third" style={{padding:"3rem",margin:"3rem"}}>
               <div className="field">
