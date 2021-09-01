841c2fe456611ffab1f7769336ad0f0d54e7d91c
Ariel Wilson <ariel.wilson.jc@gmail.com>
2021-09-01 01:05:22 -0500

    trying to fix 405 error

--- api/api.py
+++ api/api.py
@@ -171,12 +171,11 @@ def home():
 def get_current_time():
     return {'time': time.time()}
 
-#api method for Login
 @app.route('/api/login', methods=['GET', 'POST'])
 @cross_origin()
 def login():
     print(request)
-    if request.method == 'POST' or request.method == 'post' and request.is_json:    
+    if request.method == 'POST' and request.is_json:    
         email = request.json.get('email', None)
         password = request.json.get('password', None)
 
