4f3c527c8e1f9d62313a010990188ba9a12b1884
Ariel Wilson <ariel.wilson.jc@gmail.com>
2021-09-02 09:05:57 -0500

    testing api

--- src/App.js
+++ src/App.js
@@ -100,18 +100,18 @@ export default class App extends Component {
   
   login = async (email, password) => {
     
-    /*const res = await axios.post(
+    const res = await axios.post(
       '/api/login',
       { email, password },
     ).catch((res) => {
       //return { status: 401, message: 'Unauthorized' }
       console.log(res);
-    })*/
+    })
 
-    const res = await axios.get("/api/loginnow").catch((res) => {
+    /*const res = await axios.get("/api/loginnow").catch((res) => {
       //return { status: 401, message: 'Unauthorized' }
       console.log(res);
-    })
+    })*/
     console.log(res.data);
     if(res.status === 200) {
       let email  = jwt_decode(res.data.access_token).sub;
@@ -268,9 +268,9 @@ export default class App extends Component {
                     <span> <i className="fa fa-plus"></i> Add <br className="wrap-text"/> Post</span>
                   </Link>
                 )}
-                <Link to="/add-posting" className="navbar-item is-expanded is-tab">
+                {/*<Link to="/add-posting" className="navbar-item is-expanded is-tab">
                   <span> <i className="fa fa-plus"></i> Add <br className="wrap-text"/> Post</span>
-                </Link>
+                </Link>*/}
 
                 <Link onClick={ () => {this.setState({ showMenu: !this.state.showMenu })}}  to="/contact" className="navbar-item is-expanded is-tab">
                   <span> <i className="fa fa-phone"></i> Contact <br className="wrap-text"/> Us</span>
